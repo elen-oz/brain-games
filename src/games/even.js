6e4b0ed057c014bf387ed15f94c24e864b5ba6d6
Elena Ozerova <ozerova.lenka@gmail.com>
2022-08-18 16:22:14 +0200

    fix: move function isEven outside

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,12 @@ import { enginGame } from '../index.js';
 import { getRandom } from '../utilities.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => number % 2 === 0;
 
 const playGame = () => {
   const randomNumber = getRandom(1, 100);
   const questionToUser = `${randomNumber}`;
-  const isEven = () => randomNumber % 2 === 0;
+  
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
 
   return [questionToUser, correctAnswer];
