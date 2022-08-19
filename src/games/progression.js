6a92c440f1cc810561f48d614fa6cb9386c3ebb4
Elena Ozerova <ozerova.lenka@gmail.com>
2022-08-19 13:10:30 +0200

    fix: eslint

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,7 +11,7 @@ const generateProgression = (firstNumber, step, lenghtOfProgr) => {
   }
 
   return arrOfProgression;
-}
+};
 
 const generateRound = () => {
   const firstNumber = getRandom(1, 10);
